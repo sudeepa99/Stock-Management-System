a2b37a707725c342ecf86e42c988e09bebaee9c1
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-05-23 14:08:01 +0530

    admin & viewer Controller

--- backend_/index.js
+++ backend_/index.js
@@ -8,34 +8,8 @@ app.use(express.json())
 
 mongoose.connect('mongodb+srv://shashikaladilini11:shashikalanisal@cluster0.s5cehoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-  app.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-    ViewerModel.findOne({email:email}).then(viewer=>{
-        if (viewer) {
-            if (viewer.password===password) {
-                res.json("Viewer Login Successfully")
-            } else {
-                res.json("The password is incorrect")
-            }
-        } else {
-            res.json("No record existed")
-        }
-    })
-})
-app.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-    AdminModel.findOne({email:email}).then(admin=>{
-        if (admin) {
-            if (admin.password===password) {
-                res.json("Admin Login Successfully")
-            } else {
-                res.json("The password is incorrect")
-            }
-        } else {
-            res.json("No record existed")
-        }
-    })
-})
+
+
 
 
 app.listen(3001,()=>{
