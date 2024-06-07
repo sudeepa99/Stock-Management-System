542c59fdbd5cadef28415dd6f396f49904507911
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-07 23:06:10 +0530

    Packing Deatils is created

--- backend/Routes/packing.js
+++ backend/Routes/packing.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {saleDetails, packingdetails,getAllPackingDetails,categoryDetails} from "../Controllers/packingController.js";
+import {saleDetails, packingdetails,getAllPackingDetails,updatepackingdetails} from "../Controllers/packingController.js";
 
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/saledetails",saleDetails);
 router.post("/packingdetails",packingdetails);
 router.get("/getAllPacking",getAllPackingDetails);
-router.post("/categorydetails",categoryDetails);
+router.post("/:id",updatepackingdetails);
 
 export default router;
 
--- backend/models/PackingDetailsSchema.js
+++ backend/models/PackingDetailsSchema.js
@@ -1,28 +1,40 @@
 import mongoose from "mongoose";
 
-const categoryDetailsSchema = new mongoose.Schema(
+const packingDetailsShema = new mongoose.Schema(
   {
 
-   
-    details: { type: String },
-    // Fields from categoryDetails
-    teacategory: { type: String },
-    sizeofbag: { type: Number },
-    numofbags: { type: Number },
-   
+    date: {
+      type: Date,
+      required: true,
+    },
+    greenleaves: {
+      type: Number,
+      min: 0,
+      max: 999,
+    },
+    madetea: {
+      type: Number,
+      min: 0,
+      max: 999,
+    },
+    details: {
+      type: String,
+    },
+    teacategory:{
+      type: String,
+    },
+    sizeofbag: {
+      type: Number,
+      min: 0,
+      max: 999,
+    },
+    numofbags: {
+      type: Number,
+      min: 0,
+      max: 999,
+    },
 
 },
 );
-
-const packingDetailsShema = new mongoose.Schema({
-   // Fields from packingdetails
-  greenleaves: { type: Number },
-  madetea: { type: Number },
-  details: { type: String, required: true },
-  date: { type: Date,  },
-  categoryDetails: categoryDetailsSchema
-});
-
-
 
 export default mongoose.model("packingDetails", packingDetailsShema);
