1d19e91f48bcc7e0c0ec899f5467c340b65bf357
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-07 22:31:55 +0530

    categoryDetails controller and api created

--- backend/Routes/packing.js
+++ backend/Routes/packing.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {saleDetails, packingdetails,getAllPackingDetails} from "../Controllers/packingController.js";
+import {saleDetails, packingdetails,getAllPackingDetails,categoryDetails} from "../Controllers/packingController.js";
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/saledetails",saleDetails);
 router.post("/packingdetails",packingdetails);
 router.get("/getAllPacking",getAllPackingDetails);
+router.post("/categorydetails",categoryDetails);
+
 export default router;
 
