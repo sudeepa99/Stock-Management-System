c0b7d047c16322028334f1a466184ed8a1639fb8
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-18 23:57:42 +0530

    dispatch frontend

--- frontend/src/pages/Dispatch.jsx
+++ frontend/src/pages/Dispatch.jsx
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../config';
+import { toast } from 'react-toastify';
+import HashLoader from 'react-spinners/HashLoader';
 
 const Dispatch = () => {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    
+    details:'packing',
+    date: new Date().toISOString().substr(0, 10) // Set the default date to the current date
+});
+
+  
+  const navigate = useNavigate();
+
+  const handleInputChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async event => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/dispatch/dispatchdetails`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const { message } = await res.json();
+      if (!res.ok) {
+        throw new Error(message);
+      }
+      setLoading(false);
+      toast.success(message);
+      navigate('/packing2');
+    } catch (err) {
+      toast.error(err.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="Dispatch">
-      <h1>Dispatch</h1>
+    <div className="container">
+      <form className='a1' onSubmit={submitHandler}>
+        <p className='b1'>Date</p>
+        <p className='b2'>Please enter the following details to continue the process.</p>
+
+       
+        <div className="mt-7">
+          <button disabled={loading && true} type='submit'>
+            {loading ? <HashLoader size={35} color="#ffffff" /> : 'Submit'}
+          </button>
+        </div>
+      </form>
     </div>
   );
-};
+}
 
 export default Dispatch;
