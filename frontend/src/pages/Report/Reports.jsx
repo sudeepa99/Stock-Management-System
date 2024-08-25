96b55a9cc322d3d95861c49c8619c0d4e5aeebc6
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-08-25 23:46:23 +0530

    Strated to work in report screen

--- frontend/src/pages/Report/Reports.jsx
+++ frontend/src/pages/Report/Reports.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
-function Reports() {
+export default function Reports() {
   return (
-    <div className="Reports">
-      <h1>Reports22</h1>
-    </div>
-  );
-}
+    <div className='relative w-auto h-screen mt-10 mr-14 ml-80 bg-greybgColor rounded-xl'>
 
-export default Reports;
+      <h1 className='mt-6 ml-4 text-2xl text-white'>Reports</h1>
+      <h2 className='text-xl text-center text-white'>Year - Month</h2>
+      
+    </div>
+  )
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,7 +12,8 @@ export default {
         textColor: "#4E545F",
         cellwhiteColor:"#fff",
         greyColor:"#F0E6E6",
-        brownColor:"#D9D9D9"
+        brownColor:"#D9D9D9",
+        greybgColor: "#1F2327"
       },
 
       boxShadow: {
