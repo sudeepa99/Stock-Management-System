4d1be450e13b540638ac263e0e31e0a2cc80ce1c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2025-01-08 23:22:26 +0530

    Refactor dispatch routes to remove commented code and ensure middleware imports for authentication

--- backend/Routes/dispatch.js
+++ backend/Routes/dispatch.js
@@ -1,14 +1,3 @@
-// import express from "express";
-// import { dispatchDetails, findByInvoiceNo, getWeeklyDispatchDetails } from "../Controllers/dispatchController.js";
-
-// const router = express.Router();
-
-// router.post("/details", dispatchDetails);
-// router.get("/invoice/:invoicenumber", findByInvoiceNo);
-// router.get("/weekly", getWeeklyDispatchDetails);
-// export default router;
-
-
 import express from "express";
 import { dispatchDetails, findByInvoiceNo, getWeeklyDispatchDetails } from "../Controllers/dispatchController.js";
 import { authenticate, restrict } from "../utils/verifyToken.js"; // Import your middlewares
