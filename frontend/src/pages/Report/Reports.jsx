5b8ec21b76d3b9c38a2b92921decfdb3a07f8551
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2025-01-14 19:18:12 +0530

    Add totals for number and weight of bags in reports

--- frontend/src/pages/Report/Reports.jsx
+++ frontend/src/pages/Report/Reports.jsx
@@ -135,6 +135,23 @@ const Reports = () => {
 
                 )}
               </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="4" className="text-center text-gray-500 px-4 py-2">
+                    Total Number of Bags: {currentData.reduce((acc, item) => {
+                      return acc + item.numofbags;
+                    }, 0)}
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="4" className="text-center text-gray-500 px-4 py-2">
+                    Total Weight of Bags:
+                    {currentData.reduce((acc, item) => {
+                      return acc + item.numofbags * item.sizeofbag;
+                    }, 0)}
+                  </td>
+                </tr>
+              </tfoot>
             </table>
 
 
