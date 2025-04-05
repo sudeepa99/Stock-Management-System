fea27b24b949c1a44cd075c30ec85718858bab39
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-04-05 23:26:05 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -22,33 +22,40 @@ const port = process.env.PORT || 8000;
 
 // Configure CORS
 const corsOptions = {
-    origin: true,
-    // origin: process.env.CLIENT_URL || '*', // Customize the origin for security
-    // credentials: true, // Allow credentials (cookies, authorization headers)
+  origin: true,
+  // origin: process.env.CLIENT_URL || '*', // Customize the origin for security
+  // credentials: true, // Allow credentials (cookies, authorization headers)
 };
 
 // Define a simple root route for health checks
 app.get("/", (req, res) => {
-    res.status(200).send("API is working");
+  res.status(200).send("API is working");
 });
 
 // Database connection setup
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-        });
-        console.log("MongoDB connected successfully");
-    } catch (err) {
-        console.error("MongoDB connection failed:", err.message);
-        // Optional retry mechanism
-        setTimeout(connectDB, 5000);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {});
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    // Optional retry mechanism
+    setTimeout(connectDB, 5000);
+  }
 };
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // For local dev
+      "https://stock-management-system-v-git-cb3cd5-sudeepas-projects-7ded070e.vercel.app/", // Your Vercel frontend URL
+    ],
+  })
+);
 
 // Routes
 app.use("/api/v1/auth", authRoute);
@@ -60,6 +67,6 @@ app.use(errorHandler);
 
 // Start the server
 app.listen(port, async () => {
-    await connectDB();
-    console.log(`Server is running on port ${port}`);
+  await connectDB();
+  console.log(`Server is running on port ${port}`);
 });
